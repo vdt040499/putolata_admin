877cd7cdbfca00f52d39b5b8e70c563e29936b3b
Vo Tan <vdt040499@gmail.com>
2020-11-28 15:31:54 +0700

    Fix Categories and Add Product

--- src/reducers/category.reducer.js
+++ src/reducers/category.reducer.js
@@ -6,6 +6,33 @@ const initState = {
     error: null
 }
 
+const buildNewCategories = (parentId, categories, category) => {
+    let myCategories = [];
+
+    for (let cat of categories) {
+
+        if (cat._id == parentId) {
+            myCategories.push({
+                ...cat,
+                children: cat.children && cat.children.length > 0 ? buildNewCategories(parentId, [...cat.children, {
+                    _id: category._id,
+                    name: category.name,
+                    slug: category.slug,
+                    parentId: category.parentId,
+                    children: category.children
+                }], category) : []
+            });
+        } else {
+            myCategories.push({
+                ...cat,
+                children: cat.children && cat.children.length > 0 ? buildNewCategories(parentId, cat.children, category) : []
+            });
+        }
+    }
+
+    return myCategories;
+}
+
 export default (state = initState, action) => {
     console.log(action);
 
@@ -23,8 +50,13 @@ export default (state = initState, action) => {
             }
             break;
         case categoryConstants.ADD_NEW_CATEGORY_SUCCESS:
+            const category = action.payload.category;
+            const updatedCategories = buildNewCategories(category.parentId, state.categories, category);
+            console.log(updatedCategories);
+
             state = {
                 ...state,
+                categories: updatedCategories,
                 loading: false
             }
             break;
