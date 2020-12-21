3f6bd851adce0603155574a0578d5db7b9b4f98c
Vo Tan <vdt040499@gmail.com>
2020-12-22 06:22:11 +0700

    Create Page API

--- src/containers/NewPage/index.jsx
+++ src/containers/NewPage/index.jsx
@@ -4,7 +4,8 @@ import Layout from "../../components/Layout";
 import { Col, Container, Row } from "react-bootstrap";
 import Input from "../../components/UI/Input";
 import linearCategories from "../../helpers/linearCategories";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { createPage } from "../../actions";
 
 const NewPage = (props) => {
   const [createModal, setCreateModal] = useState(false);
@@ -12,29 +13,65 @@ const NewPage = (props) => {
   const category = useSelector((state) => state.category);
   const [categories, setCategories] = useState([]);
   const [categoryId, setCategoryId] = useState("");
-  const [desc, setDesc] = useState('');
+  const [desc, setDesc] = useState("");
+  const [type, setType] = useState("");
   const [banners, setBanners] = useState([]);
   const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     setCategories(linearCategories(category.categories));
   }, [category]);
 
+  const onCategoryChange = (e) => {
+    console.log(e.target.value);
+    const category = categories.find(
+      (category) => category._id == e.target.value
+    );
+    setCategoryId(e.target.value);
+    setType(category.type);
+  };
+
+  const handleBannerImages = (e) => {
+    console.log(e);
+    setBanners([...banners, e.target.files[0]]);
+  };
+
+  const handleProductImages = (e) => {
+    console.log(e);
+    setProducts([...products, e.target.files[0]]);
+  };
+
+  const submitPageForm = (e) => {
+    // e.target.preventDefault();
+
+    if (title === "") {
+      alert("Title is required");
+      setCreateModal(false);
+      return;
+    }
+
+    const form = new FormData();
+    form.append("title", title);
+    form.append("description", desc);
+    form.append("category", categoryId);
+    form.append("type", type);
+    banners.forEach((banner, index) => {
+      form.append("banners", banner);
+    });
+    products.forEach((product, index) => {
+      form.append("products", product);
+    });
+
+    dispatch(createPage(form));
+  };
+
   const renderCreatePageModal = () => {
-
-    const handleBannerImages = (e) => {
-        console.log(e);
-    }
-
-    const handleProductImages = (e) => {
-        console.log(e);
-    }
-
     return (
       <Modal
         show={createModal}
         modalTitle={"Create New Page"}
-        handleClose={() => setCreateModal(false)}
+        handleClose={submitPageForm}
       >
         <Container>
           <Row>
@@ -42,7 +79,7 @@ const NewPage = (props) => {
               <select
                 className="form-control"
                 value={categoryId}
-                onChange={(e) => setCategoryId(e.target.value)}
+                onChange={onCategoryChange}
               >
                 <option value="">select category</option>
                 {categories.map((cat) => (
@@ -69,23 +106,47 @@ const NewPage = (props) => {
             <Col>
               <Input
                 value={desc}
-                onChange={(e) => setTitle(e.target.value)}
+                onChange={(e) => setDesc(e.target.value)}
                 placeholder={"Page Desc"}
                 className=""
               />
             </Col>
           </Row>
 
+          {banners.length > 0
+            ? banners.map((banner, index) => (
+                <Row key={index}>
+                  <Col>{banner.name}</Col>
+                </Row>
+              ))
+            : null}
           <Row>
-              <Col>
-                <input className="form-control" type="file" name="banner" onChange={handleBannerImages}/>
-              </Col>
+            <Col>
+              <Input
+                className="form-control"
+                type="file"
+                name="banner"
+                onChange={handleBannerImages}
+              />
+            </Col>
           </Row>
 
+          {products.length > 0
+            ? products.map((product, index) => (
+                <Row key={index}>
+                  <Col>{product.name}</Col>
+                </Row>
+              ))
+            : null}
           <Row>
-              <Col>
-                <input className="form-control" type="file" name="products" onChange={handleProductImages}/>
-              </Col>
+            <Col>
+              <Input
+                className="form-control"
+                type="file"
+                name="products"
+                onChange={handleProductImages}
+              />
+            </Col>
           </Row>
         </Container>
       </Modal>
